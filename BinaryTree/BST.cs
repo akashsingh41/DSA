cda29c6eb84bded3a9858e70f3ee932b75c85884
Akash Singh <Akash.Singh@allscripts.com>
2022-05-06 13:57:33 +0530

    finding Diameter of a BST

---
+++ BinaryTree/BST.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BST
+    {
+        public Node root;
+
+        public BST()
+        {
+            root = null;
+        }
+
+        public void AddNode(int x)
+        {
+            Node new_node = new Node(x);
+            if (root == null)
+            {
+                root = new_node;
+            }
+            else
+            {
+                Node current, parent;
+                current = root;
+                while (true)
+                {
+                    parent = current;
+                    if (current == null)
+                    {
+                        current = new_node;
+                        break;
+                    }
+
+                    if (x < current.value)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = new_node;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = new_node;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
