0b940c49be7b9958da1c208061cc92a4599ad37c
Akash Singh <Akash.Singh@allscripts.com>
2022-05-06 19:20:58 +0530

    In Order, Pre Order, Post order Traversal in BST

---
+++ TreeTraversal/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeTraversal 
+{
+    public class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int i)
+        {
+            value = i;
+        }
+
+        public Node()
+        {
+        }
+
+        public int Display()
+        {
+            return value;
+        }
+    }
+}
---
+++ TreeTraversal/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TreeTraversal
+{
+    class Program
+    {
+        public static void InOrder(Node root) 
+        {
+            if (root != null)
+            {
+                InOrder(root.left);
+                Console.Write($"{root.Display()} ");
+                InOrder(root.right);
+            }
+        }
+
+        public static void PreOrder(Node root)
+        {
+            if (root != null)
+            {
+                Console.Write($"{root.Display()} ");
+                PreOrder(root.left);                
+                PreOrder(root.right);
+            }
+        }
+
+        public static void PostOrder(Node root)
+        {
+            if (root != null)
+            {                
+                PostOrder(root.left);
+                PostOrder(root.right);
+                Console.Write($"{root.Display()} ");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            BST tree = new BST();
+            tree.AddNode(23);
+            tree.AddNode(45);
+            tree.AddNode(16);
+            tree.AddNode(37);
+            tree.AddNode(3);
+            tree.AddNode(99);
+            tree.AddNode(22);
+            Console.WriteLine("In-Order Traversal:-");
+            InOrder(tree.root);
+
+            Console.WriteLine("\nPre-Order Traversal:-");
+            PreOrder(tree.root);
+
+            Console.WriteLine("\nPost-Order Traversal:-");
+            PostOrder(tree.root);
+
+        }
+    }
+}
