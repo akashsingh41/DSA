7b82c937a95ab1654c8cba6d8df4c3fc9da4c363
Akash Singh <Akash.Singh@allscripts.com>
2022-05-10 17:59:35 +0530

    iterative traversal

---
+++ IterativeInOrderTraversal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BinaryTree;
+using System.Collections.Generic;
+namespace IterativeInOrderTraversal
+{
+    class Program
+    {
+        static void IterativeInOrder(Node root)
+        {
+            Stack<Node> stack = new Stack<Node>();
+            Node current = root;
+
+            while (current != null || stack.Count > 0)
+            {
+                while (current != null)
+                {
+                    stack.Push(current);
+                    current = current.left;
+                }
+                current = stack.Pop();
+                Console.Write(current.value + " ");
+                current = current.right;
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            BT tree = new BT();
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+            
+            IterativeInOrder(tree.root);
+        }
+    }
+}
---
+++ IterativePreOrderTraversal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using BinaryTree;
+
+namespace IterativePreOrderTraversal
+{
+    class Program
+    {
+        static void IterativePreOrder(Node root) 
+        {
+            if (root == null) return;
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(root);
+            while(stack.Count > 0)
+            {
+                Node current = stack.Pop();
+                Console.Write(current.value + " ");
+                if (current.right != null)
+                    stack.Push(current.right);
+                if (current.left != null)
+                    stack.Push(current.left);
+            }
+        }
+        static void Main(string[] args)
+        {
+            BT tree = new BT();
+            tree.root = new Node(10);
+            tree.root.left = new Node(8);
+            tree.root.right = new Node(2);
+            tree.root.left.left = new Node(3);
+            tree.root.left.right = new Node(5);
+            tree.root.right.left = new Node(2);
+            IterativePreOrder(tree.root);
+        }
+    }
+}
