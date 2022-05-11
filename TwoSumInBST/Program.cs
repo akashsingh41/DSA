8ddb45b3857b8a17bdaacdc1beb8beeab3ba8ff7
Akash Singh <Akash.Singh@allscripts.com>
2022-05-11 15:08:38 +0530

    vertical sum in BST

---
+++ TwoSumInBST/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using BinarySearchTree;
+namespace TwoSumInBST
+{
+    class Program
+    {
+        static void InOrder(Node n)
+        {
+            if (n == null) return;
+            InOrder(n.left);
+            nodelist.Add(n.value);
+            InOrder(n.right);
+        }
+        static void PrintPairs(int sum, Node root)
+        {
+            nodelist.Clear();
+            InOrder(root);
+            
+            HashSet<int> hs = new HashSet<int>();
+            foreach(int x in nodelist)
+            {
+                if (hs.Contains(sum - x))
+                    Console.WriteLine($"{sum} sum pair: {x} & {sum-x}");
+                hs.Add(x);
+            }
+        }
+
+        static List<int> nodelist = new List<int>();
+        static void Main(string[] args)
+        {
+            BST tree = new BST();
+            int[] numbers = { 21, 56, 33, 76, 24, 12, 4 };
+            foreach (int x in numbers)
+                tree.InsertNode(x);
+
+            PrintPairs(80, tree.root);
+        }
+    }
+}
---
+++ VerticalSumInBST/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using BinarySearchTree;
+
+namespace VerticalSumInBST
+{
+    class Program
+    {
+        static void Utility(Node n, int hd, Dictionary<int,int> sum)
+        {
+            if (n == null)
+                return;
+            
+            Utility(n.left, hd - 1, sum);
+            
+            if (sum.ContainsKey(hd))
+                sum[hd] = sum[hd] + n.value;
+            else
+                sum.Add(hd, n.value);
+
+            Utility(n.right, hd + 1, sum);
+        }
+      
+        static Dictionary<int,int> CalculateVerticalSums(Node node)
+        {
+            if (node == null) return null;
+            Dictionary<int, int> sum = new Dictionary<int, int>();
+            
+            Utility(node, 0, sum);
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            BST tree = new BST();
+            int[] nodes = { 4, 2, 3, 1, 5 };
+            foreach (int x in nodes) { tree.InsertNode(x); }
+
+            Console.WriteLine("Following are the values of vertical sums with the positions of the columns with respect to root :");
+            foreach (KeyValuePair<int, int> keyValue in CalculateVerticalSums(tree.root))            
+            {
+                Console.WriteLine($"Distance: {keyValue.Key}, Sum:{keyValue.Value}");
+            }
+        }
+    }
+}
