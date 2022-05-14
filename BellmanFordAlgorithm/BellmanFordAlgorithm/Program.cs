a790ba3d9a9b60659e2a0952ee1379395bc645ed
Akash Singh <Akash.Singh@allscripts.com>
2022-05-15 00:12:44 +0530

    bellmanford, kruskal,kosaraju

---
+++ BellmanFordAlgorithm/BellmanFordAlgorithm/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace BellmanFordAlgorithm
+{
+    public class Graph
+    {
+        // A class to represent a weighted edge in graph
+        public class Edge
+        {
+            public int src, dest, weight;
+            public Edge()
+            {
+                src = dest = weight = 0;
+            }
+        }
+
+        public int V, E;
+        public Edge[] edge;
+
+        // Creates a graph with V vertices and E edges
+        public Graph(int v, int e)
+        {
+            V = v;
+            E = e;
+            edge = new Edge[e];
+            for (int i = 0; i < e; ++i)
+                edge[i] = new Edge();
+        }
+
+        public void BellmanFord(Graph graph, int src)
+        {
+            int V = graph.V, E = graph.E;
+            int[] dist = new int[V];
+
+            // Step 1: Initialize distances from src to all other vertices as INFINITE
+            for (int i = 0; i < V; ++i)
+                dist[i] = int.MaxValue;
+            dist[src] = 0;
+
+            // Step 2: Relax all edges |V| - 1 times. A simple shortest path from src to any other vertex can have at-most |V| - 1 edges
+            for (int i = 1; i < V; ++i)
+            {
+                for (int j = 0; j < E; ++j)
+                {
+                    int u = graph.edge[j].src;
+                    int v = graph.edge[j].dest;
+                    int weight = graph.edge[j].weight;
+                    if (dist[u] != int.MaxValue && dist[u] + weight < dist[v])
+                        dist[v] = dist[u] + weight;
+                }
+            }
+
+            // Step 3: check for negative-weight cycles. The above step guarantees shortest distances if graph doesn't contain negative weight cycle. If we get a shorter path, then there is a cycle.
+            for (int j = 0; j < E; ++j)
+            {
+                int u = graph.edge[j].src;
+                int v = graph.edge[j].dest;
+                int weight = graph.edge[j].weight;
+                if (dist[u] != int.MaxValue && dist[u] + weight < dist[v])
+                {
+                    Console.WriteLine("Graph contains negative weight cycle");
+                    return;
+                }
+            }
+            printArr(dist, V);
+        }
+
+        void printArr(int[] dist, int V)
+        {
+            Console.WriteLine("Vertex \t Distance from Source");
+            for (int i = 0; i < V; ++i)
+                Console.WriteLine(i + "\t\t" + dist[i]);
+        }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int V = 5; // Number of vertices in graph
+            int E = 8; // Number of edges in graph
+
+            Graph graph = new Graph(V, E);
+
+
+            graph.edge[0].src = 0;
+            graph.edge[0].dest = 1;
+            graph.edge[0].weight = -1;
+
+
+            graph.edge[1].src = 0;
+            graph.edge[1].dest = 2;
+            graph.edge[1].weight = 4;
+
+
+            graph.edge[2].src = 1;
+            graph.edge[2].dest = 2;
+            graph.edge[2].weight = 3;
+
+
+            graph.edge[3].src = 1;
+            graph.edge[3].dest = 3;
+            graph.edge[3].weight = 2;
+
+
+            graph.edge[4].src = 1;
+            graph.edge[4].dest = 4;
+            graph.edge[4].weight = 2;
+
+
+            graph.edge[5].src = 3;
+            graph.edge[5].dest = 2;
+            graph.edge[5].weight = 5;
+
+
+            graph.edge[6].src = 3;
+            graph.edge[6].dest = 1;
+            graph.edge[6].weight = 1;
+
+
+            graph.edge[7].src = 4;
+            graph.edge[7].dest = 3;
+            graph.edge[7].weight = -3;
+
+            graph.BellmanFord(graph, 0);
+        }
+    }
+}
---
+++ StronglyConnectedGraph/StronglyConnectedGraph/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StronglyConnectedGraph
+{
+    internal class Graph
+    {
+        private int V;
+        private List<List<int>> adj;
+
+        public Graph(int v)
+        {
+            this.V = v;
+            adj = new List<List<int>>();
+            for (int i = 0; i < v; i++)
+            {
+                adj.Add(new List<int>());
+            }
+        }
+
+        public void addEdge(int u, int v) { adj[u].Add(v); }
+
+        void DFS(int v, bool[] visited)
+        {
+            visited[v] = true;
+            foreach (int neighbour in adj[v])
+            {
+                if (!visited[neighbour])
+                    DFS(neighbour, visited);
+            }
+        }
+        Graph getTranspose()
+        {
+            Graph g = new Graph(V);
+            for (int v = 0; v < V; v++)
+            {
+                foreach(int n in adj[v])
+                // Recur for all the vertices adjacent to this vertex
+                    g.adj[n].Add(v);
+            }
+            return g;
+        }
+        public bool IsSC()
+        {
+            bool[] visited = new bool[V];
+            // Step 1: Mark all the vertices as not visited(For first DFS)
+            for (int i = 0; i < V; i++)
+                visited[i] = false;
+
+            // Step 2: Do DFS traversal starting from first vertex.
+            DFS(0, visited);
+
+            // If DFS traversal doesn't visit all vertices, then return false.
+            for (int i = 0; i < V; i++)
+                if (visited[i] == false)
+                    return false;
+
+            // Step 3: Create a reversed graph
+            Graph transpose = getTranspose();
+
+            // Step 4: Mark all the vertices as not visited (For second DFS)
+            for (int i = 0; i < V; i++)
+                visited[i] = false;
+
+            // Step 5: Do DFS for reversed graph starting from first vertex. Starting Vertex must be same starting point of first DFS
+            transpose.DFS(0, visited);
+
+            // If all vertices are not visited in second DFS, then return false
+            for (int i = 0; i < V; i++)
+                if (visited[i] == false)
+                    return false;
+
+            return true;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g1 = new Graph(5);
+            g1.addEdge(0, 1);
+            g1.addEdge(1, 2);
+            g1.addEdge(2, 3);
+            g1.addEdge(3, 0);
+            g1.addEdge(2, 4);
+            g1.addEdge(4, 2);
+           
+            Console.WriteLine(g1.IsSC()?"Yes":"No");
+
+
+            Graph g2 = new Graph(4);
+            g2.addEdge(0, 1);
+            g2.addEdge(1, 2);
+            g2.addEdge(2, 3);
+            Console.WriteLine(g2.IsSC() ? "Yes" : "No");
+
+        }
+    }
+}
